c6ac98140bd37cdb2d83596276c5f14783261b0b
Asadullohbek05 <ayubovasadbek33@gmail.com>
2024-06-27 15:59:14 +0500

    fixed mapping

--- src/data/Cards.js
+++ src/data/Cards.js
@@ -1,7 +1,7 @@
 let Cards = [
   {
     name: "Войнич Дарья",
-    image: "../../assets/images/ourteam-img1.png",
+    image: "ourteam-img1.png",
     description: "Заместитель директора по продажам",
     phoneNumber1: "+375 (17) 270-53-77 (115)",
     phoneNumber2: "+375 29 112-73-48",
@@ -10,7 +10,7 @@ let Cards = [
   },
   {
     name: "Мисько Екатерина",
-    image: "../../assets/images/ourteam-img2.png",
+    image: "ourteam-img2.png",
     description: "Начальник отдела сопровождения",
     phoneNumber1: "+375 (17) 270-53-77 (115)",
     phoneNumber2: " +375 29 112-73-48",
@@ -19,7 +19,7 @@ let Cards = [
   },
   {
     name: "Дмитроченко Дмитрий",
-    image: "../../assets/images/ourteam-img3.png",
+    image: "ourteam-img3.png",
     description: "Начальник отдела допечатной подготовки",
     phoneNumber1: "+375 (17) 270-53-77 (333)",
     phoneNumber2: " +375 29 360-32-26",
@@ -28,7 +28,7 @@ let Cards = [
   },
   {
     name: "Антух Евгений",
-    image: "../../assets/images/ourteam-img4.png",
+    image: "ourteam-img4.png",
     description: "Начальник отдела снабжения",
     phoneNumber1: "+375 (17) 270-53-77 (148)",
     phoneNumber2: " +375 44 764-16-28",
@@ -37,7 +37,7 @@ let Cards = [
   },
   {
     name: "Мисник Елена",
-    image: "../../assets/images/ourteam-img5.png",
+    image: "ourteam-img5.png",
     description: "Специалист по работе с клиентами",
     phoneNumber1: "+375 (17) 270-53-77 (322)",
     phoneNumber2: " +375 29 329-34-03",
